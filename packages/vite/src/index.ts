2ee0bf46cd8d0026ecc61a26c892d28a617cd825
snowingfox <1503401882@qq.com>
2022-09-18 15:34:58 +0800

    feat: set max listener number

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,7 +1,13 @@
+import type { IncomingMessage } from 'http'
+import type http from 'http'
+import events from 'events'
 import type { Plugin } from 'vite'
 import httpProxy from 'http-proxy'
+import parse from 'url-parse'
 import { transformJSONToTs } from './logic'
 import type { OhmytsOptions } from './types'
+
+const ee = events.EventEmitter
 
 export function ohmytsVite(
   options: OhmytsOptions | OhmytsOptions[],
@@ -9,17 +15,26 @@ export function ohmytsVite(
   return {
     name: 'ohmyts',
     configureServer(server) {
+      const proxy = httpProxy.createProxyServer()
+
+      const proxyWeb = (req: IncomingMessage, res: http.ServerResponse, pathname: string, option: any) => {
+        proxy.web(req, res, {
+          changeOrigin: true,
+          ...option.proxyOptions,
+        })
+      }
+
+      ee.setMaxListeners(0)
+
       return () => {
-        const proxy = httpProxy.createProxyServer()
         if (!Array.isArray(options))
           options = [options]
 
         for (const option of options) {
           server.middlewares.use(option.target, (req, res) => {
-            proxy.web(req, res, {
-              changeOrigin: true,
-              ...option.proxyOptions,
-            })
+            const pathname = parse(req.url!).pathname
+
+            proxyWeb(req, res, pathname, option)
 
             proxy.on('proxyRes', (proxyRes) => {
               const chunks: Uint8Array[] = []
